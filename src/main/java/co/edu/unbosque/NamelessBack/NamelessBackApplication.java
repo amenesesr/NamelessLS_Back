e7b5559f288f3cea574d0da6c60d817e14581542
Alejandro Meneses <amenesesr82@gmail.com>
2021-09-27 15:34:09 -0500

    se agregaron los camabios de la semana 5 de temas de seguridad

--- src/main/java/co/edu/unbosque/NamelessBack/NamelessBackApplication.java
+++ src/main/java/co/edu/unbosque/NamelessBack/NamelessBackApplication.java
@@ -2,8 +2,9 @@ package co.edu.unbosque.NamelessBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  {SecurityAutoConfiguration.class})
 public class NamelessBackApplication {
 
 	public static void main(String[] args) {
